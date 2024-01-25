f4870d8c4c67ad3c327567b3632da4f8d3c0caba
ASKAR <askarjon.abdullayev@gmail.com>
2024-01-25 11:51:48 +0500

    user list wait time reduced

--- project/urls.py
+++ project/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls')),
     path('', include('apps.gym.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
