f46112da163d73585394e54c9da59a02c00ca02d
Shokhdiyor04 <shoxdiyor04@gmail.com>
2024-02-20 22:04:35 +0500

    frontend changes

--- project/settings.py
+++ project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
