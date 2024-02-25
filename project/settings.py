6a60bf1394eff2688035c4f1865bc884ca07608d
ASKAR <askarjon.abdullayev@gmail.com>
2024-02-25 16:17:23 +0500

    dynamic database engines

--- project/settings.py
+++ project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
