6a600aaafafe724baa3a0613b3a1e04d1b19b2db
ASKAR <askarjon.abdullayev@gmail.com>
2024-03-25 00:37:25 +0500

    dir

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -21,10 +21,10 @@ def generate_and_save_access(user_id):
     )
     qr.add_data({"access_code": code})
     qr.make(fit=True)
-
-    qr_code_name = f"media/access_qr/{user.gym.name}/{code}.png"
-    if not os.path.exists(qr_code_name):
-        os.makedirs(qr_code_name)
+    qr_dir = f"media/access_qr/{user.gym.name}/"
+    if not os.path.exists(qr_dir):
+        os.makedirs(qr_dir)
+    qr_code_name = f"{qr_dir}{code}.png"
     img = qr.make_image(fill_color="white", back_color="black")
     img.save(qr_code_name)
     access = Access.objects.create(
